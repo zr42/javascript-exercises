3de3eec263c7196749bc5caf907dd52c48a4cb95
zr42 <zacharyrejwan242@gmail.com>
2023-02-25 17:51:49 -0600

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) 
+{
+    let stringArray = string.split('');
+    stringArray.reverse();
+    string = stringArray.join('');
+    return string;
 };
 
 // Do not edit below this line
