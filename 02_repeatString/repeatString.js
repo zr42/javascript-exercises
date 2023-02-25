4c8354a267a2e3bb43ae60d12bad03629c1aeb17
zr42 <zacharyrejwan242@gmail.com>
2023-02-25 17:31:36 -0600

    Finish first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) 
+{
+    let result = '';
+    if(num < 0)
+    {
+        return 'ERROR';
+    }
+    for(let i = 0; i < num; i++)
+    {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
