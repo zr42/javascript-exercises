ac950de25f1a16e7ce224dfee94ce84559b87e98
zr42 <zacharyrejwan242@gmail.com>
2023-02-25 18:47:25 -0600

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) 
+{
+    let sum = 0;
+    lowEnd = Math.min(start, end);
+    highEnd = Math.max(start, end);
+    if(lowEnd < 0)
+    {
+        return 'ERROR';
+    }
+    if(typeof start != 'number' || typeof end != 'number')
+    {
+        return 'ERROR';
+    }
 
+    for(let i = lowEnd; i <= highEnd; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
