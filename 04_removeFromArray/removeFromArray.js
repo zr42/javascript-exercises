5928e634a0878074eeaff762a68cf2eb1eb17b01
zr42 <zacharyrejwan242@gmail.com>
2023-02-25 18:21:31 -0600

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(initialArray, ...toRemove) 
+{
+    for(let i = 0;i < toRemove.length; i++)
+    {
+        let target = initialArray.indexOf(toRemove[i]);
+        if(target != -1)
+        {
+            initialArray.splice(target, 1);
+        }
+    }
+    return initialArray;
 };
 
 // Do not edit below this line
